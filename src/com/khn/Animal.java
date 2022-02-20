467dc9f67894ee2c72d466c112578117e89daa86
gss <gss@gss.com>
2022-02-20 09:09:00 +0800

    set and get

--- src/com/khn/Animal.java
+++ src/com/khn/Animal.java
@@ -2,4 +2,39 @@ package com.khn;
 
 public class Animal {
     String sex;
+    String id;
+    String name;
+
+    public Animal(String sex, String id, String name) {
+        this.sex = sex;
+        this.id = id;
+        this.name = name;
+    }
+
+    public Animal() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
